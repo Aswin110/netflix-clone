a734b94d1b57f590566353e4e49ad92bac3cf98b
Aswin110 <aswinashok110@gmail.com>
2023-07-06 04:15:18 +0530

    add side navigation icons
    import icons from vue-material-desgin-icons

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <script setup>
     import { onMounted, ref } from 'vue';
 
-    import Magnify from 'netflix-clone/node_modules/vue-material-design-icons/Magnify';
-    import HomeOutline from 'netflix-clone/node_modules/vue-material-design-icons/HomeOutline';
-    import TrendingUp from 'netflix-clone/node_modules/vue-material-design-icons/TrendingUp';
-    import Television from 'netflix-clone/node_modules/vue-material-design-icons/Television';
-    import MovieOutline from 'netflix-clone/node_modules/vue-material-design-icons/MovieOutline';
-    import Plus from 'netflix-clone/node_modules/vue-material-design-icons/Plus';
-    import ChevronLeft from 'netflix-clone/node_modules/vue-material-design-icons/ChevronLeft';
+    import Magnify from 'vue-material-design-icons/Magnify.vue';
+    import HomeOutline from 'vue-material-design-icons/HomeOutline.vue'
+    import TrendingUp from 'vue-material-design-icons/TrendingUp.vue'
+    import Television from 'vue-material-design-icons/Television.vue'
+    import MovieOutline from 'vue-material-design-icons/MovieOutline.vue'
+    import Plus from 'vue-material-design-icons/Plus.vue'
+    import ChevronLeft from 'vue-material-design-icons/ChevronLeft.vue'
 
     import {useMovieStore} from './stores/movie'
     import { storeToRefs } from 'pinia'
@@ -16,8 +16,30 @@
 </script>
 
 <template>
-  <div class="text-red-400">
-    text here
+  <div class="fixed w-full h-screen bg-black"> 
+    <div v-if="!showFullVideo" id="sideNav" class="flex z-40 items-center w-[120p] h-screen bg-black relative">
+      N
+      <div>
+        <div class="py-2 mx-10 my-6">
+          <Magnify fillColor="#ffffff" :size="40" class="cursor-pointer" />
+        </div>
+        <div class="py-2 mx-10 my-6 border-b-4 border-b-red-500">
+          <HomeOutline fillColor="#ffffff" :size="40" class="cursor-pointer" />
+        </div>
+        <div class="py-2 mx-10 my-6">
+          <TrendingUp fillColor="#ffffff" :size="40" class="cursor-pointer" />
+        </div>
+        <div class="py-2 mx-10 my-6">
+          <Television fillColor="#ffffff" :size="40" class="cursor-pointer" />
+        </div>
+        <div class="py-2 mx-10 my-6">
+          <MovieOutline fillColor="#ffffff" :size="40" class="cursor-pointer" />
+        </div>
+        <div class="py-2 mx-10 my-6">
+          <Plus fillColor="#ffffff" :size="40" class="cursor-pointer" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
